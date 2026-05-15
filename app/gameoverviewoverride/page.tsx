17355c48077f6855b9513a06f6abbaa7a15ff500
SushiTheCoder <serpentmc67@gmail.com>
2026-05-15 14:04:06 -0400

    Fixed Issue Where Override Game Overview Linked To Push Back Herobot

--- app/gameoverviewoverride/page.tsx
+++ app/gameoverviewoverride/page.tsx
@@ -603,7 +603,7 @@ export default function GameOverview() {
           </div>
           </Link>
 
-          <Link href="/Herobot" style={navButtonStyle}
+          <Link href="/Herobotoverride" style={navButtonStyle}
             onMouseEnter={(e) => {
               e.currentTarget.style.background = 'white'; 
               e.currentTarget.style.color = '#ff8c00';    
